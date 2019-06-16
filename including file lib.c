d65e4f2bd0ebef25b9871f5a7c8d21e9fb23910d
srilatha nampally <51399274+srilathanampally29@users.noreply.github.com>
2019-06-16 15:04:31 +0530

    Add files via upload

---
+++ including file lib.c
@@ -0,0 +1,18 @@
+/*
+ * including file lib.c
+ *
+ * Created: 14-06-2019 10:12:28
+ * Author : DELL
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
